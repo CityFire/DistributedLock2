4c8c13bfcbfef2e347ccdfc6fdabd120fab50d68
CityFire <13787697167@163.com>
2023-04-13 00:32:03 +0800

    代码生成器

---
+++ src/main/java/com/wjc/distributedlock/projo/Generator.java
@@ -0,0 +1,11 @@
+package com.wjc.distributedlock.projo;
+
+import com.baomidou.mybatisplus.generator.AutoGenerator;
+
+public class Generator {
+    public static void main(String[] args) {
+//        AutoGenerator autoGenerator = new AutoGenerator();
+//        autoGenerator
+//        autoGenerator.execute();
+    }
+}
